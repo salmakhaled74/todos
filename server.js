7d349b35fe359f6bd9ae49225d1decb3c1196b12
Salma Khaled <salma.khaled963@gmail.com>
2023-06-01 09:10:04 +0300

    modified:   server.js

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use('/public', express.static('public', {
   }
 }))
 
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
